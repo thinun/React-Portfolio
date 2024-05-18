25e84c44a512975ab71e0c054e7aa11dd750ae57
thinun <75312054+thinun@users.noreply.github.com>
2024-05-19 00:24:16 +0530

    projects buttons

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -9,19 +9,30 @@ import {faLinkedin} from "@fortawesome/free-brands-svg-icons";
 
 const Hero = () => {
     return (
-        <div className="hero" id={"home"}>
-            <img src={profile_img} alt={"Profile Image"}/>
-            <h1><span>I'm Thinun Tharushika,</span> Full-Stack Developer</h1>
-            <p>From the assembly line to the code line, I'm driven by innovation. Eagerly seeking an internship to
-                infuse creativity, contribute fresh perspectives, and ignite groundbreaking projects.</p>
-            <div className={"hero-buttons"}>
-                <a className={"hero-btn-links"} href={"https://github.com/thinun"}><i><FontAwesomeIcon icon={faGithub}
-                                                                                                       className="icon"/></i></a>
-                <a className={"hero-btn-links"} href={"Thinun-CV.pdf"}><i><FontAwesomeIcon icon={faFile} className="icon"/></i></a>
-                <a className={"hero-btn-links"} href={"https://www.linkedin.com/in/thinun-tharushika/"}> <i><FontAwesomeIcon icon={faLinkedin} className="icon"/></i></a>
+        <>
+            <div className="hero" id={"home"}>
+
+                <img src={profile_img} alt={"Profile Image"}/>
+                <div className={"animation"}></div>
+
+                <h1><span>I'm Thinun Tharushika, </span> Full-Stack Developer</h1>
+
+                <p>From the assembly line to the code line, I'm driven by innovation. Eagerly seeking an internship
+                    to
+                    infuse creativity, contribute fresh perspectives, and ignite groundbreaking projects.</p>
+                <div className={"hero-buttons"}>
+                    <a className={"hero-btn-links"} href={"https://github.com/thinun"}><i><FontAwesomeIcon
+                        icon={faGithub}
+                        className="icon"/></i></a>
+                    <a className={"hero-btn-links"} href={"Thinun-CV.pdf"}><i><FontAwesomeIcon icon={faFile}
+                                                                                               className="icon"/></i></a>
+                    <a className={"hero-btn-links"} href={"https://www.linkedin.com/in/thinun-tharushika/"}>
+                        <i><FontAwesomeIcon icon={faLinkedin} className="icon"/></i></a>
+                </div>
             </div>
-        </div>
-    );
+        </>
+    )
+        ;
 };
 
 export default Hero;
