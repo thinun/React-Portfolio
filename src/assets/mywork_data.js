8bdcb33788f3b1f75fa9320c6ab90b8ae5eb4ee2
thinun <75312054+thinun@users.noreply.github.com>
2024-06-10 20:02:34 +0530

    cv added

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -4,50 +4,58 @@ import project3_img from '../assets/pokemon Search app.png'
 import project4_img from '../assets/rockpaperS.png'
 import project5_img from '../assets/roman number converter.png'
 import project6_img from '../assets/Tp number validater.png'
+import project7_img from '../assets/recipe_finder.png'
 
 const mywork_data = [
     {
-        w_no:1,
-        w_name:"E-Commerce Website",
-        w_img:project1_img,
-        w_url:"https://thinun.github.io/Ecommercec-website/",
-        g_hub:"https://github.com/thinun/Ecommercec-website"
+        w_no: 1,
+        w_name: "E-Commerce Website",
+        w_img: project1_img,
+        w_url: "https://thinun.github.io/Ecommercec-website/",
+        g_hub: "https://github.com/thinun/Ecommercec-website"
 
     },
     {
-        w_no:2,
-        w_name:"Todo List",
-        w_img:project2_img,
-        w_url:"https://thinun.github.io/ToDo-List/",
-        g_hub:"https://github.com/thinun/ToDo-List"
+        w_no: 2,
+        w_name: "Todo List",
+        w_img: project2_img,
+        w_url: "https://thinun.github.io/ToDo-List/",
+        g_hub: "https://github.com/thinun/ToDo-List"
     },
     {
-        w_no:3,
-        w_name:"Pokemon Search app",
-        w_img:project3_img,
-        w_url:"https://thinun.github.io/Pokemon-Search-App/",
+        w_no: 3,
+        w_name: "Pokemon Search app",
+        w_img: project3_img,
+        w_url: "https://thinun.github.io/Pokemon-Search-App/",
         g_hub: "https://github.com/thinun/Pokemon-Search-App"
     },
     {
-        w_no:4,
-        w_name:"Rock Paper Scissors",
-        w_img:project4_img,
-        w_url:"https://thinun.github.io/Rock-Paper-Scissors/"
+        w_no: 4,
+        w_name: "Rock Paper Scissors",
+        w_img: project4_img,
+        w_url: "https://thinun.github.io/Rock-Paper-Scissors/"
     },
     {
-        w_no:5,
-        w_name:"Roman Numerical Converter",
-        w_img:project5_img,
-        w_url:"https://thinun.github.io/Roman-Numeral-Converter/",
-        g_hub:"https://github.com/thinun/Rock-Paper-Scissors"
+        w_no: 5,
+        w_name: "Roman Numerical Converter",
+        w_img: project5_img,
+        w_url: "https://thinun.github.io/Roman-Numeral-Converter/",
+        g_hub: "https://github.com/thinun/Rock-Paper-Scissors"
     },
     {
-        w_no:6,
-        w_name:"Telephone Number Validator",
-        w_img:project6_img,
-        w_url:"https://thinun.github.io/Telephone-Number-Validator/",
-        g_hub:"https://github.com/thinun/Telephone-Number-Validator"
+        w_no: 6,
+        w_name: "Telephone Number Validator",
+        w_img: project6_img,
+        w_url: "https://thinun.github.io/Telephone-Number-Validator/",
+        g_hub: "https://github.com/thinun/Telephone-Number-Validator"
+    },
+    {
+        w_no: 6,
+        w_name: "Recipe Finder",
+        w_img: project7_img,
+        w_url: "https://github.com/thinun/Recipe-Finder",
+        g_hub: "https://github.com/thinun/Recipe-Finder"
     },
 ]
- 
+
 export default mywork_data;
